d1bd0e158591fc8e3e342ae0ff413c38ca8f1491
AyushiChopra02 <139689074+AyushiChopra02@users.noreply.github.com>
2023-11-17 01:21:33 +0530

    add

---
+++ Hashing/freqArray.cpp
@@ -0,0 +1,28 @@
+//time O(N*2Q)
+//space: O(N)
+#include<iostream>
+#include <vector>
+using namespace std ;
+void solve(){
+    int n ,q ;
+    cin >> n >>q ;
+
+//vector array ki trh e hota h main diff ki store sequntial data  static in nature jayda stre krta h
+//Declaration : 	
+//vector< object_type > vector_variable_name;
+
+    vector<int> arr(n+1);
+    for(int i=1 ; i<=n ; i++){
+        cin >> arr[i];
+    }
+    vector<int> frequency(n+1 , 0);
+    for(int i=1 ; i<=n ; i++){
+        frequency[arr[i]]++ ;
+    }
+    for(int i=1 ; i<=q ;i++){
+        int x ;
+        cin >> x;
+        cout << frequency[x] << "\n" ;
+    }
+    
+}
---
+++ Hashing/freqMap.cpp
@@ -0,0 +1,24 @@
+//time O(N+Q)
+#include<iostream>
+#include <unordered_map>
+#include <vector>
+using namespace std ;
+void solve(){
+    int n ,q ;
+    cin >> n >>q ;
+    vector<int> arr(n+1);
+    for(int i=1 ; i<=n ; i++){
+        cin >> arr[i];
+    }
+    //mapping
+    unordered_map<int , int> frequencyMap ;
+    for(int i=1 ; i<=n ; i++){
+        frequencyMap[arr[i]]++ ;
+    }
+    for(int i=1 ; i<=q ; i++){
+        int x ;
+        cin >> x ;
+        cout << frequencyMap[x] << "\n";
+    }
+    }
+    
