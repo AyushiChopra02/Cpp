a1e3601046063cce9aa57ff89a32672cc6f35fd7
AyushiChopra02 <139689074+AyushiChopra02@users.noreply.github.com>
2023-11-19 00:23:48 +0530

    add

---
+++ Hashing/PrefixSum.cpp
@@ -0,0 +1,28 @@
+//Time compl = O(N+Q)
+//space com =  O(N)
+#include<iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std ;
+void solve(){
+    int n,q ;
+    cin >> n;
+    vector<int> arr(n+1);
+    for(int i =1 ; i<=n ; i++){
+        cin >> arr[i];
+
+    }
+    vector<int> prefixSum(n+1 , 0);
+    for(int i=1 ; i<=n ; i++){
+        prefixSum[i] = prefixSum[i-1] + arr[i];
+
+    }
+    cin >> q;
+    for(int i=1 ; i<=q ; i++){
+        int l , r;
+        cin >> l >> r ;
+        l++ ;
+        r++ ;
+        cout << prefixSum[r] - prefixSum[l-1] << "\n";
+    }
+}
