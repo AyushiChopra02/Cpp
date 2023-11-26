007127c4b160da095f098a98771f672c21a163a9
AyushiChopra02 <139689074+AyushiChopra02@users.noreply.github.com>
2023-11-26 18:54:10 +0530

    add

--- Hashing/CountPairWithGiven sum.cpp
+++ Hashing/CountPairWithGiven sum.cpp
@@ -6,7 +6,7 @@ using namespace std  ;
 class Solution{
     public:
     int getPairsCount(int arr[] , int n  , int k){
-        unordered_map<int , int> frequency;
+        unordered_map<int , int> frequency ; 
         int ans = 0;
         for(int i = 0 ; i<n ; i++ ){
             int b  = arr[i];
