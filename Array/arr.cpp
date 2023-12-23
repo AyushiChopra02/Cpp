5928912f18a247f3e84ec4b3351428249ac35d93
AyushiChopra02 <139689074+AyushiChopra02@users.noreply.github.com>
2023-12-23 23:15:31 +0530

    add

--- Array/arr.cpp
+++ Array/arr.cpp
@@ -9,17 +9,17 @@
     cout << endl << "evrythng is fine" << endl ;
 
 //pahli do 2 7 ayegi aur bchi hue hmne btai nai to 0 aayegi
-    int third[15] = {2,7};
+    int third[15] = {2,7} ;
     int n= 15 ;
     cout <<    "print" << endl ;
     for(int i = 0 ; i<n ; i++){
         cout << third[i] << " " ;
     }
     //pahle postn one aayegi baki sb 0
-    int fifth[10] = {1};
+    int fifth[10] = {1}  ;
      n=10 ;
     cout << "print" <<endl ;
     for(int i=0; i<n ; i++ ){
-        cout << fifth[i] << " ";
+        cout << fifth[i] << " " ;
     }
  }
